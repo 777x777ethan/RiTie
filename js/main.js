2cff659fd7195c42caf05d001b965dc003b811ea
777x777ethan <39490292+777x777ethan@users.noreply.github.com>
2018-07-08 18:14:16 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,7 +4,9 @@ function block_show(){
     document.getElementById("main_block").style.visibility="hidden";
     document.getElementById("choose_block").style.visibility="visible";
     var body=document.getElementById("body");
-    var i=253
+    var styles=window.getComputedStyle(body);
+    var color=styles.getPropertyValue("background-Color");
+    var i=parseInt(color[4]+color[5]+color[6]);
     var bodyId=window.setInterval(function(){
         if(i<=60 || run==0){
             window.clearInterval(bodyId);
@@ -13,13 +15,38 @@ function block_show(){
         i--;
         body.style.backgroundColor="rgb("+i+","+i+","+i+")";
     },5);
+    var type_width=document.getElementsByClassName("c_type");
+    var x=0;
+    var type_widthId=window.setInterval(function(){
+        if(x>=120 || run==0){
+            var type_margin=document.getElementsByClassName("c_type");
+            var margins=window.getComputedStyle(type_margin[1]);
+            var margin=margins.getPropertyValue("margin");
+            var m=parseInt(margin[5]+margin[6]);
+            if(m<=10){
+                for(var i=0;i < type_margin.length;i++){
+                    type_margin[i].style.margin="20px 10px";
+                }
+            }
+            window.clearInterval(type_widthId);
+            return;
+        }
+        x++;
+        for(var i=0;i < type_width.length;i++){
+            type_width[i].style.width=x+"px";
+        }
+    },1);
+    
 }
+
 function block_close(){
     run=0;
     document.getElementById("choose_block").style.visibility="hidden";
     document.getElementById("main_block").style.visibility="visible";
     var body=document.getElementById("body");
-    var i=60
+    var styles=window.getComputedStyle(body);
+    var color=styles.getPropertyValue("background-Color");
+    var i=parseInt(color[4]+color[5]+color[6]);
     var bodyId=window.setInterval(function(){
         if(i>=253|| run==1){
             window.clearInterval(bodyId);
@@ -28,4 +55,5 @@ function block_close(){
         i++;
         body.style.backgroundColor="rgb("+i+","+i+","+i+")";
     },5);
+
 }
