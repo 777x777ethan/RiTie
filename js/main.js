9f6865687aa4d0866b6722b5ef2f9660c3af20aa
777x777ethan <39490292+777x777ethan@users.noreply.github.com>
2018-07-09 21:48:24 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,11 +1,20 @@
+function getElementById_Value(id,type){
+    var ids=document.getElementById(id);
+    var styles=window.getComputedStyle(ids);
+    return(styles.getPropertyValue(type));
+}
+function getElementByClass_Value(classes,type){
+    var classs=document.getElementsByClassName(classes);
+    var styles=window.getComputedStyle(classs[0]);
+    return(styles.getPropertyValue(type));
+}
 var run=1;
 function block_show(){
     run=1;
+    document.getElementById("cblock_type").style.visibility="visible";
     document.getElementById("main_block").style.visibility="hidden";
     document.getElementById("choose_block").style.visibility="visible";
-    var body=document.getElementById("body");
-    var styles=window.getComputedStyle(body);
-    var color=styles.getPropertyValue("background-Color");
+    var color=getElementById_Value("body","background-Color");
     var i=parseInt(color[4]+color[5]+color[6]);
     var bodyId=window.setInterval(function(){
         if(i<=60 || run==0){
@@ -20,8 +29,7 @@ function block_show(){
     var type_widthId=window.setInterval(function(){
         if(x>=120 || run==0){
             var type_margin=document.getElementsByClassName("c_type");
-            var margins=window.getComputedStyle(type_margin[1]);
-            var margin=margins.getPropertyValue("margin");
+            var margin=getElementByClass_Value("c_type","margin");
             var m=parseInt(margin[5]+margin[6]);
             if(m<=10){
                 for(var i=0;i < type_margin.length;i++){
@@ -31,7 +39,7 @@ function block_show(){
             window.clearInterval(type_widthId);
             return;
         }
-        x++;
+        x=x+2;
         for(var i=0;i < type_width.length;i++){
             type_width[i].style.width=x+"px";
         }
@@ -41,11 +49,13 @@ function block_show(){
 
 function block_close(){
     run=0;
+    var type_width=document.getElementsByClassName("c_type");
+    for(var h=0;h < type_width.length;h++){
+        type_width[h].style.height=60+"px";
+    }
     document.getElementById("choose_block").style.visibility="hidden";
     document.getElementById("main_block").style.visibility="visible";
-    var body=document.getElementById("body");
-    var styles=window.getComputedStyle(body);
-    var color=styles.getPropertyValue("background-Color");
+    var color=getElementById_Value("body","background-Color");
     var i=parseInt(color[4]+color[5]+color[6]);
     var bodyId=window.setInterval(function(){
         if(i>=253|| run==1){
@@ -60,4 +70,23 @@ function block_close(){
         body.style.backgroundColor="rgb("+i+","+i+","+i+")";
     },5);
 
+}
+
+function c_type_act(act){
+    var bodywidth=parseInt(getElementById_Value("cblock_type","width"));
+    var bodyheight=parseInt(getElementById_Value("cblock_type","height"));
+    var actid=document.getElementById(act.id);
+    var x=parseInt(getElementByClass_Value("c_type","width"));
+    var y=parseInt(getElementByClass_Value("c_type","height"));
+    var actidtime=window.setInterval(function(){
+        if(x>=bodywidth||y>=bodyheight|| run==0){
+            document.getElementById("cblock_type").style.visibility="hidden";
+            window.clearInterval(actidtime);
+            return;
+        }
+        x=x+3;
+        y=y+4;
+        actid.style.height= y+"px";
+        actid.style.width= x+"px";
+    },1)
 }
